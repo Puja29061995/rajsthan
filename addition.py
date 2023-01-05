d3be21a0abf0f847422353ef99543665928247d0
lakhan009 <121679225+lakhan009@users.noreply.github.com>
2023-01-05 18:36:43 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,9 @@
+print("This is addition file")
+
+
+def get_addition(a,b):
+    print(f"a == {a} , b == {b}")
+    add = a + b
+    print(f"Addition of {a} and {b} is {add}")
+    print("*"*50)
+    return add
