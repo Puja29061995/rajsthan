69d13a4918218e68afb5490296d05552b3a402db
lakhan009 <121679225+lakhan009@users.noreply.github.com>
2023-01-08 23:12:24 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,11 @@
+list1 = [2,3,4,5]
+square_list = []
+
+for num in list1:
+    square_list.append(num ** 2)
+
+<<<<<<< HEAD
+print("squarelist: ", square_list)
+=======
+print((square_list))
+>>>>>>> feature1
